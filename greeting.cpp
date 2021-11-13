d9c75c7c96ee06636980463062262b90c02d6402
rasyidzkun <rasyidzkun@gmail.com>
2021-11-13 20:48:49 +0700

    initial commit

---
+++ greeting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "What is your name : ";
+    cin >> name;
+
+    cout << "Hello " << name << ", nice to meet you";
+    return 0;
+}
