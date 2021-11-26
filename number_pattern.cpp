3e2024068d33c47655123184b36cd27222611d22
rasyidzkun <rasyidzkun@gmail.com>
2021-11-26 09:58:50 +0700

    add program to print number pattern

---
+++ number_pattern.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter an integer :";
+    cin >> n;
+
+    cout << endl;
+    cout << "Pattern 1\n";
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << setw(3) << i << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+    cout << "Pattern 2\n";
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j >= i; j--){
+            cout << setw(3) << i << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+    cout << "Pattern 3\n";
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << setw(3) << j << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+    cout << "Pattern 4\n";
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j >= i; j--){
+            cout << setw(3) << j << " ";
+        }
+        cout << endl;
+    }
+
+    system("pause>0");
+    return 0;
+}
