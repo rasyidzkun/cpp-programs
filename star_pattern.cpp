3c1c4238724080e3e70483e3dbfeef41860457f9
rasyidzkun <rasyidzkun@gmail.com>
2021-12-03 09:09:15 +0700

    add star pattern 4

--- star_pattern.cpp
+++ star_pattern.cpp
@@ -39,6 +39,18 @@ int main(){
         cout << endl;
     }
 
+    cout << endl;
+    cout << "Pattern 4 \n";
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j < i; j++){
+            cout << " ";
+        }
+        for(int k = n; k >= i; k--){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
     system("pause>0");
     return 0;
 }
