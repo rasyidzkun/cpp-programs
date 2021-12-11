d8000cb79968cf4d6f2333d4e3ff6de0e89e035c
rasyidzkun <rasyidzkun@gmail.com>
2021-12-11 08:07:56 +0700

    add factorial program using recursion

---
+++ factorial_recursion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n);
+
+int main(){
+    int n;
+    
+    cout << "Enter a positive integer : ";
+    cin >> n;
+
+    cout << "Factorial of " << n << " = " << factorial(n);
+
+    system("pause>0");
+    return 0;
+}
+
+int factorial(int n){
+    if(n > 1){
+        return n * factorial(n - 1);
+    }
+    return 1;
+}
